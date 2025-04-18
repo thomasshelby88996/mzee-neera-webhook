ca72e3c7131fc469d9a95c53fd5a86b4e9995cc8
thomasshelby88996 <thomasshelby88996@gmail.com>
2025-04-18 05:52:34 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,41 +1,29 @@
-from flask import Flask, request
 import os
-import dialogflow_v2 as dialogflow
+from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+from google.cloud import dialogflow_v2 as dialogflow
 import json
-from google.oauth2 import service_account
 
 app = Flask(__name__)
-
-# Set up credentials and project ID
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "dialogflow-service-key.json"
 DIALOGFLOW_PROJECT_ID = "mzeeneera2026bot"
-SESSION_ID = "current-user-id"
+session_client = dialogflow.SessionsClient()
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    incoming_msg = request.values.get("Body", "").strip()
-    response = MessagingResponse()
-    reply = response.message()
-
+    incoming_msg = request.values.get('Body', '')
+    sender_id = request.values.get('From', '')
+    session = session_client.session_path(DIALOGFLOW_PROJECT_ID, sender_id)
+    text_input = dialogflow.TextInput(text=incoming_msg, language_code="en")
+    query_input = dialogflow.QueryInput(text=text_input)
     try:
-        session_client = dialogflow.SessionsClient()
-        session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
-
-        text_input = dialogflow.types.TextInput(text=incoming_msg, language_code="en")
-        query_input = dialogflow.types.QueryInput(text=text_input)
-
-        dialogflow_response = session_client.detect_intent(
-            request={"session": session, "query_input": query_input}
-        )
-
-        reply_text = dialogflow_response.query_result.fulfillment_text
-        reply.body(reply_text)
-
+        response = session_client.detect_intent(request={"session": session, "query_input": query_input})
+        bot_reply = response.query_result.fulfillment_text
     except Exception as e:
-        reply.body("Something went wrong while contacting Dialogflow.")
-
-    return str(response)
+        bot_reply = "Something went wrong while contacting Dialogflow."
+    twilio_resp = MessagingResponse()
+    twilio_resp.message(bot_reply)
+    return str(twilio_resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
